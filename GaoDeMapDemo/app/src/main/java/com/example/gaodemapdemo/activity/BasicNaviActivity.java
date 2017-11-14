88178071fd11eb237e5dcbd29f4085bc94d5db67
maryzml222 <2505508361@qq.com>
2017-11-14 12:44:55 +0800

    add project

---
+++ GaoDeMapDemo/app/src/main/java/com/example/gaodemapdemo/activity/BasicNaviActivity.java
@@ -0,0 +1,22 @@
+package com.example.gaodemapdemo.activity;
+
+import android.os.Bundle;
+
+import com.amap.api.navi.AMapNaviView;
+import com.example.gaodemapdemo.R;
+
+
+public class BasicNaviActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_basic_navi);
+        mAMapNaviView = (AMapNaviView) findViewById(R.id.navi_view);
+        mAMapNaviView.onCreate(savedInstanceState);
+        mAMapNaviView.setAMapNaviViewListener(this);
+    }
+
+
+}
